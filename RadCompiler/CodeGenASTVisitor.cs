1b9bb30df9a164b6ea66fc7298c4f35f786b8a15
John Louderback <john@powereddynamics.com>
2022-08-13 22:22:20 -0700

    Added compiler project for compiling through LLVM

---
+++ RadCompiler/CodeGenASTVisitor.cs
@@ -0,0 +1,108 @@
+﻿using LLVMSharp.Interop;
+using RadParser;
+using RadParser.AST.Node;
+using Void = RadParser.AST.Node.Void;
+
+namespace RadCompiler; 
+
+public class CodeGenASTVisitor : BaseASTVisitor<LLVMBuilderRef> {
+  
+  private readonly LLVMModuleRef module;
+
+  private readonly LLVMBuilderRef builder;
+
+  private readonly Dictionary<string, LLVMValueRef> namedValues = new Dictionary<string, LLVMValueRef>();
+  
+  public override LLVMBuilderRef Visit(BinaryOperation node) {
+    throw new NotImplementedException();
+  }
+
+
+  public override LLVMBuilderRef Visit(FunctionCallExpression node) {
+    throw new NotImplementedException();
+  }
+
+
+  public override LLVMBuilderRef Visit(FunctionDeclaration node) {
+    throw new NotImplementedException();
+  }
+
+
+  public override LLVMBuilderRef Visit(FunctionScopeStatement node) {
+    throw new NotImplementedException();
+  }
+
+  public override LLVMBuilderRef Visit(LiteralExpression node) {
+    throw new NotImplementedException();
+  }
+
+
+  public override LLVMBuilderRef Visit(Modifier node) {
+    throw new NotImplementedException();
+  }
+
+
+  public override LLVMBuilderRef Visit(Module node) {
+    throw new NotImplementedException();
+  }
+
+
+  public override LLVMBuilderRef Visit(NamedTypeParameter node) {
+    throw new NotImplementedException();
+  }
+
+
+  public override LLVMBuilderRef Visit(NumericLiteral node) {
+    throw new NotImplementedException();
+  }
+
+
+  public override LLVMBuilderRef Visit(OperationalKeyword node) {
+    throw new NotImplementedException();
+  }
+
+
+  public override LLVMBuilderRef Visit(Operator node) {
+    throw new NotImplementedException();
+  }
+
+
+  public override LLVMBuilderRef Visit(PositionalParameter node) {
+    throw new NotImplementedException();
+  }
+
+
+  public override LLVMBuilderRef Visit(ReferenceExpression node) {
+    throw new NotImplementedException();
+  }
+
+
+  public override LLVMBuilderRef Visit(Statement node) {
+    throw new NotImplementedException();
+  }
+
+
+  public override LLVMBuilderRef Visit(TopLevel node) {
+    throw new NotImplementedException();
+  }
+
+
+  public override LLVMBuilderRef Visit(TypeIdentifier node) {
+    throw new NotImplementedException();
+  }
+
+
+  public override LLVMBuilderRef Visit(TypeReference node) {
+    throw new NotImplementedException();
+  }
+
+
+  public override LLVMBuilderRef Visit(Value node) {
+    throw new NotImplementedException();
+  }
+
+
+  public override LLVMBuilderRef Visit(Void node) {
+    throw new NotImplementedException();
+  }
+}
---
+++ RadCompiler/Compiler.cs
@@ -0,0 +1,15 @@
+﻿using LLVMSharp;
+using LLVMSharp.Interop;
+
+namespace RadCompiler;
+
+public class Compiler {
+  public unsafe void Compile() {
+    // Creates the application. A module is the whole program, not just a single file. 
+    var module = LLVMModuleRef.CreateWithName("my cool jit");
+    // Creates a context, such as for a thread.
+    var context = LLVMContextRef.Create();
+    // Creates a "builder" for generating IR code.
+    var builder = LLVMBuilderRef.Create(context);
+  }
+}
